ce36fb0360e05e83f7d08b6dff0bd746f9b5ebbe
james willett <jms.wlltt@gmail.com>
2017-09-08 00:48:53 -0400

    this is my first commit, cmoney

---
+++ index.js
@@ -0,0 +1,92 @@
+
+const express = require("express");
+const app = express();
+
+const multer = require("multer");
+const upload = multer();
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'James',
+  host: 'localhost',
+  database: 'socialnews'
+})
+
+app.use(express.static('public'));
+/*
+var links = [
+  {
+    author: 'james',
+    title: 'Facebook',
+    url: 'http://www.facebook.com/',
+    favorite: false,
+  },{
+    author: 'james',
+    title: 'Wikipedia',
+    url: 'http://www.wikipedia.com/',
+    favorite: false,
+  },{
+    author: 'james',
+    title: 'Reddit',
+    url: 'http://wwww.reddit.com/',
+    favorite: false,
+  }
+];*/
+
+
+
+app.post("/deletelink", jsonParser, (request, response) => {
+  pool.query('delete from links where id=$1',[request.body.index])
+  response.json(request.body.index);
+})
+
+app.get("/getlinks", (request,response) => {
+  pool.query('select * from links', (err, links) => {
+    if (err){
+      console.log(err)
+    }
+
+    response.json(links.rows);
+  })
+})
+
+app.post("/addlinks", jsonParser, (request,response) => {
+
+  pool.query('insert into links (author, title, url) values ($1, $2, $3)',[request.body.author, request.body.title, request.body.url], (err, links) => {
+
+    if (err){
+      console.log(err)
+    }
+    console.log(links.rows);
+    response.json({});
+  })
+
+})
+
+app.post("/favorite", jsonParser, (request, response) => {
+  pool.query('select favorite from links where id=$1',[request.body.index], (err, link) => {
+    const { favorite } = link.rows[0];
+    pool.query('update links set favorite=$1 where id=$2',[!favorite, request.body.index], (err, updatedLink) => {
+      response.json({favorite: !favorite});
+    })
+  })
+})
+
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
---
+++ public/js/publink.js
@@ -0,0 +1,203 @@
+const buttonEl = document.getElementById('submitButton');
+const submitFormEl = document.createElement('form');
+const submitDivEl = document.getElementById('submitDiv')
+const contentEl = document.getElementById('content');
+submitDivEl.appendChild(submitFormEl);
+
+buttonEl.addEventListener('click', () => {
+  buttonEl.disabled = true;
+
+  const authorField = document.createElement('input');
+    authorField.className = "link"
+    authorField.setAttribute('placeholder','Author');
+    authorField.style.width='25%';
+    authorField.setAttribute('name','author');
+    authorField.setAttribute('required','true');
+    submitFormEl.appendChild(authorField);
+
+  const titleField = document.createElement('input');
+    titleField.className = 'link';
+    titleField.setAttribute('placeholder','Link Title');
+    titleField.style.width='25%';
+    titleField.setAttribute('name','title');
+    titleField.setAttribute('required','true');
+    submitFormEl.appendChild(titleField);
+
+  const urlField = document.createElement('input');
+    urlField.className = 'link';
+    urlField.setAttribute('placeholder','Link URL');
+    urlField.style.width='40%';
+    urlField.setAttribute('name','url');
+    urlField.setAttribute('required','true');
+    submitFormEl.appendChild(urlField);
+
+  const linkSubmit = document.createElement('input');
+    linkSubmit.setAttribute("class","btn btn-default navbar-btn")
+    linkSubmit.setAttribute('type','submit')
+    linkSubmit.setAttribute('id','addLink')
+    linkSubmit.style.width='10%';
+    linkSubmit.style.height='3em';
+    linkSubmit.style.backgroundColor = '#66f';
+    linkSubmit.style.color = "#fff";
+    //linkSubmit.textContent = 'Add Link';
+
+  submitFormEl.appendChild(linkSubmit);
+
+  submitFormEl.addEventListener('submit', function submitFormFunc(e){
+    e.preventDefault();
+    buttonEl.disabled = false;
+    const author = e.target.elements.author.value;
+    const title = e.target.elements.title.value;
+    const url = e.target.elements.url.value;
+
+    submitFormEl.innerHTML='';
+    var urlChecked = '';
+
+    if (  !url.toLowerCase().startsWith("http://") &&
+          !url.toLowerCase().startsWith("https://")){
+      urlChecked = "http://".concat(url);
+    } else {
+      urlChecked = url;
+    }
+
+    const newLink = {author, title, url:urlChecked, favorite:false};
+
+    fetch(
+      "/addlinks",{
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newLink)
+      })
+      .then(response => response.json())
+      .then(result => {
+        //console.log(result);
+        submitFormEl.removeEventListener('submit', submitFormFunc);
+        const successEl = document.createElement('div');
+          successEl.setAttribute('class','successEl');
+          successEl.textContent = `Added ${title} successfully!!`
+          submitDivEl.appendChild(successEl);
+
+        setTimeout(() => {
+          submitDivEl.removeChild(successEl);
+          //links = links.concat([newLink]);
+          contentEl.innerHTML = '';
+          getLinks();
+        } , 3000);
+      })
+
+
+  });
+});
+
+const printLinks = ({id: index, author, title, url, favorite}) => {
+  console.log(index);
+  const linkContainer = document.createElement('div');
+    linkContainer.setAttribute('class','link');
+    contentEl.prepend(linkContainer);//<==
+
+  const deleteMe = document.createElement('a');
+    deleteMe.textContent = '✖';
+    deleteMe.setAttribute('href','#');
+    deleteMe.setAttribute('class','deleteMe');
+    linkContainer.appendChild(deleteMe);
+    deleteMe.addEventListener('click', e => {
+      console.log(title);
+      const r = confirm(`Are you sure you want to delete "${title}"?`);
+      if ( r ) {
+        fetch("/deletelink",{
+          method: "POST",
+          body: JSON.stringify({index}),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => response.json())
+          .then(result => {
+            console.log(result)
+            deleteMe.parentNode.remove();
+          })
+        }
+        //links.splice(index, 1);
+        //console.log(index);
+      //deleteMe.parentNode.parentNode.innerHTML = '';
+      //links.map(printLinks)
+
+  })
+
+  const favEl = document.createElement('a');
+    if(favorite){
+      favEl.textContent = '❤️';
+    } else {
+      favEl.textContent = '💔';
+    }
+    favEl.setAttribute('href','#');
+    favEl.setAttribute('class','fave');
+    linkContainer.appendChild(favEl);
+    favEl.addEventListener('click', e => {
+      fetch("/favorite",{
+        method: "POST",
+        body: JSON.stringify({index}),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      })
+        .then(response => response.json())
+        .then(result => {
+          console.log(result)
+          if(result.favorite){
+            favEl.textContent = '❤️';
+          } else {
+            favEl.textContent = '💔';
+          }
+      })
+    })
+
+  const linkDevEl = document.createElement('div');
+    linkDevEl.setAttribute('class','link');
+    linkDevEl.style.paddingLeft = '20px'
+    linkDevEl.style.paddingTop = '0px'
+    linkContainer.appendChild(linkDevEl);
+
+  const linkHeadline = document.createElement('h4');
+    linkHeadline.setAttribute('class','linkHeadline')
+    linkDevEl.appendChild(linkHeadline);
+
+  const linkTitle = document.createElement('a');
+    linkTitle.textContent = title;
+    linkTitle.setAttribute('class','linkTitle');
+    linkTitle.setAttribute('href', url);
+    linkTitle.setAttribute('target','_blank')
+    linkHeadline.appendChild(linkTitle);
+
+  const linkUrl = document.createElement('span');
+    linkUrl.textContent = url;
+    linkUrl.setAttribute('class','linkUrl')
+    linkHeadline.appendChild(linkUrl);
+
+  const linkAuthor = document.createElement('span');
+    linkAuthor.textContent = `Submitted by ${author}`;
+    linkAuthor.setAttribute('class','linkAuthor');
+    linkDevEl.appendChild(linkAuthor);
+}
+
+const getLinks = () => {
+  fetch( "/getlinks",{
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      result.map(printLinks)
+    })
+}
+
+getLinks();
